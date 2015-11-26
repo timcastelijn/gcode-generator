ddd5a5b52b7735bb8d6704074a8c94237f6145d6
timcastelijn <t.castelijn@hotmail.com>
2015-11-26 16:39:01 +0100

    converted template to 3d gcoder

--- g_code3d.py
+++ g_code3d.py
@@ -6,22 +6,25 @@
 
 import rhinoscriptsyntax as rs
 import math
-import sys
+import os
 import Rhino
-
-
-def getDirectory():
-    myDir = ""
-    for item in sys.path:
-        myDir = myDir + item
-    return myDIr
-
-def read_config():
-        
-    dir = '/Users/timcastelijn/Library/Application Support/McNeel/Rhinoceros/scripts/'
-    # dir = getDirectory()
     
-    f = open(dir + 'config.ini')
+file = '' 
+sfeed = 0
+sspindle = 0
+
+# save offset from the workpiece
+Z_OFFSET = 2 #[mm]
+   
+
+# read a configfile in the specified directory
+def readConfig():
+        
+    #get the script file dir
+    dir = os.getcwd()
+    
+    
+    f = open(dir + '/config.ini')
     lines = f.readlines()
     f.close()
     par ={}
@@ -40,13 +43,81 @@ def SaveFileName(title=None, filter=None, folder=None, filename=None, extension=
     if extension: fd.DefaultExt = extension
     if fd.ShowDialog(): return fd.FileName
 
-def write_G(path, par):
-    #
-    # G code output
-    #
-    
+def writeFastMoveToCurveStart(curve):
+    # fast move to path start
+    pt = rs.CurveStartPoint(curve)
+    file.write("G00 X%0.4f"%pt.X+" Y%0.4f"%pt.Y+" Z%0.4f"%Z_OFFSET+"\n")
 
+def writePlungeToCurveStart(curve):
+    # fast move to path start
+    pt = rs.CurveStartPoint(curve)
+    file.write("G01 X%0.4f"%pt.X+" Y%0.4f"%pt.Y+" Z%0.4f"%pt.Z+"\n")        
+        
+def writePlungeRetract(curve):
+    # fast move to path start
+    pt = rs.CurveEndPoint(curve)
+    file.write("G00 X%0.4f"%pt.X+" Y%0.4f"%pt.Y+" Z%0.4f"%Z_OFFSET+"\n")          
+        
+def writeSpindleEnable():
+    file.write("M03\n") # spindle on clockwise
+
+def writeSpindleDisable():
+    file.write("M05\n") # spindle on clockwise
+
+def writeReturnToHomingPos():
+    file.write("G00 X0.0000 Y0.0000 F1000\n")        
+
+def writeHeader(file, par):
+
+    sfeed           = par['feedrate_cut']
+    sspindle        = par['intensity_cut']
     
+    # write header
+    file.write("G90\n") # absolute positioning
+    file.write("F"+str(sfeed)+"\n") # feed rate
+    file.write("S"+str(sspindle)+"\n") # spindle speed
+    file.write("M08\n") # coolant on     
+     
+def writePolyline(curve):  
+    points = rs.CurvePoints(curve)
+    for pt in points:
+        file.write("G01 X%0.4f"%pt.X+" Y%0.4f"%pt.Y+" Z%0.4f"%pt.Z+"\n")
+     
+def writeArc(curve):
+            
+    normal = rs.CurveTangent(curve, 0)
+            
+    # get curvepoints
+    startpt     = rs.CurveStartPoint(curve)
+    endpt       = rs.CurveEndPoint(curve)
+    midpt       = rs.ArcCenterPoint(curve)
+            
+    # calc G2/G3 parameters
+    x   = endpt.X
+    y   = endpt.Y
+    i   = -startpt.X + midpt.X
+    j   = -startpt.Y + midpt.Y
+        
+    # make a distinction between positive and negative direction
+    if ((normal[1] > 0) and (startpt.X > midpt.X)) or ((normal[1] < 0) and (startpt.X < midpt.X) or (normal[1]==0 and (normal[0]==1 or normal[0] ==-1) and startpt.X == midpt.X)):
+        file.write("G03 X%0.4f"%x+" Y%0.4f"%y+" I%0.4f"%i+" J%0.4f"%j +"\n")
+    else:
+        file.write("G02 X%0.4f"%x+" Y%0.4f"%y+" I%0.4f"%i+" J%0.4f"%j +"\n")    
+
+def writeCurve(curve):
+                        
+    polyLine    = rs.ConvertCurveToPolyline(curve, a_tolerance, tolerance)
+    points      = rs.CurvePoints(polyLine)
+            
+    # insert division points as line
+    for pt in points:
+        file.write("G01 X%0.4f"%pt.X+" Y%0.4f"%pt.Y+" Z%0.4f"%pt.Z+"\n")
+    
+    # remove objects after use
+    rs.DeleteObjects(polyLine)
+
+def writeG(selection, par):
+
     # get variables
     sfeed           = par['feedrate_cut']
     efeed           = par['feedrate_engrave']
@@ -59,88 +130,39 @@ def write_G(path, par):
     
     filename = SaveFileName ("Save", "Toolpath Files (*.nc)|*.nc||")
         
-    
+    # retrun function if no filename was specified
     if not filename: return
-    
-        
+
+    global file 
     file = open(filename, 'w')
     
+    writeHeader(file, par)
+    writeSpindleEnable()
     
-    # write header
-    file.write("G90\n") # absolute positioning
-    file.write("F"+str(sfeed)+"\n") # feed rate
-    file.write("S"+str(sspindle)+"\n") # spindle speed
-    file.write("M08\n") # coolant on
-    for curve in path:
-        
-        # fast move to path start
-        pt = rs.CurveStartPoint(curve)
-        file.write("G00 X%0.4f"%pt.X+" Y%0.4f"%pt.Y+"\n")
-        file.write("M03\n") # spindle on clockwise
-        
-        # change feedrate for engraving
-        if (rs.ObjectLayer(curve) == "engrave"):
-            file.write("F%0.1f"%efeed + "\n")
-            file.write("S%0.1f"%e_intensity + "\n")
-        else:
-            file.write("F%0.1f"%sfeed + "\n")
-            file.write("S%0.1f"%sspindle + "\n")
-        
+    for curve in selection:
+
+        #move to start of the curve at height 0+offset
+        writeFastMoveToCurveStart(curve)
+        writePlungeToCurveStart(curve)
+
         # detect type of curve for different G-codes
         if (rs.IsPolyline(curve)) or rs.IsLine(curve):
-
-            points = rs.CurvePoints(curve)
-            for pt in points:
-                file.write("G01 X%0.4f"%pt.X+" Y%0.4f"%pt.Y+"\n")
+            writePolyline(curve)
         
-        elif rs.IsArc(curve):
-            normal = rs.CurveTangent(curve, 0)
-            
-            # get curvepoints
-            startpt     = rs.CurveStartPoint(curve)
-            endpt       = rs.CurveEndPoint(curve)
-            midpt       = rs.ArcCenterPoint(curve)
-            
-            # calc G2/G3 parameters
-            x   = endpt.X
-            y   = endpt.Y
-            i   = -startpt.X + midpt.X
-            j   = -startpt.Y + midpt.Y
-            
-            # make a distinction between positive and negative direction
-            if ((normal[1] > 0) and (startpt.X > midpt.X)) or ((normal[1] < 0) and (startpt.X < midpt.X) or (normal[1]==0 and (normal[0]==1 or normal[0] ==-1) and startpt.X == midpt.X)):
-#                file.write(";positive ARC ccw \n")
-                file.write("G03 X%0.4f"%x+" Y%0.4f"%y+" I%0.4f"%i+" J%0.4f"%j +"\n")
-            else:
-#                file.write(";negative ARC cw \n")
-                file.write("G02 X%0.4f"%x+" Y%0.4f"%y+" I%0.4f"%i+" J%0.4f"%j +"\n")
+        # elif rs.IsArc(curve):
+        #     writeArc(curve)
  
         else:
-            print "curve detected, subdiv needed"
-            
-            #rs.ConvertCurveToPolyline(segment,angle_tolerance=5.0, tolerance=0.01, delete_input=False)
-            polyLine    = rs.ConvertCurveToPolyline(curve, a_tolerance, tolerance)
-            points      = rs.CurvePoints(polyLine)
-            
-            # insert division points as line
-            for pt in points:
-                file.write("G01 X%0.4f"%pt.X+" Y%0.4f"%pt.Y+"\n")
-            # remove objects after use
-            rs.DeleteObjects(polyLine)
-                
-        file.write("M05\n") # spindle stop
-    
-    file.write("G00 X0.0000 Y0.0000 F1000\n")
-    # file.write("M09\n") # coolant off
-    # file.write("M30\n") # program end and reset
-    file.close()
+            writeCurve()
+        
+        # return to offset at end of the curve
+        writePlungeRetract(curve)
 
-    # rs.MessageBox("file succesfully saved to: " + filename + ", with the following parameters:\n"+
-    #               "cut feedrate: %0.1f"%sfeed+"\n"+
-    #               "cut intensity: %0.1f"%sspindle+"\n"+
-    #               "engrave feedrate: %0.1f"%efeed+"\n"+
-    #               "engrave intensity: %0.1f"%e_intensity+"\n"
-    #               )
+    # return to homing position and turn of the spindle
+    writeReturnToHomingPos()
+    writeSpindleDisable()  
+    
+    file.close()
 
 
 # Check to see if this file is being executed as the "main" python
@@ -148,8 +170,8 @@ def write_G(path, par):
 # This allows us to use the module which ever way we want.
 if( __name__ == "__main__" ):
     
-    par = read_config()
+    par = readConfig()
     
-    path = rs.GetObjects("Select Curves/polylines/arcs/circles", rs.filter.curve, True, True)
+    selection = rs.GetObjects("Select Curves/polylines/arcs/circles", rs.filter.curve, True, True)
     
-    write_G(path, par)
+    writeG(selection, par)
